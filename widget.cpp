9867193d635e90f0f177bf1c08b23d3722fa5203
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2015-09-25 20:07:43 +0200

    dc+oc rendering, lut per cube

--- widget.cpp
+++ widget.cpp
@@ -24,15 +24,6 @@ widget::widget() {
     resize(gpucubeedge * supercubeedge, gpucubeedge * supercubeedge);
 }
 
-widget::~widget() {
-    makeCurrent();
-    for(auto & layer : layers) {
-        for (auto & texture : boost::make_iterator_range(layer.textures.data(), layer.textures.data() + layer.textures.num_elements())) {
-            delete texture;
-        }
-    }
-}
-
 void widget::initializeGL() {
     std::ios_base::sync_with_stdio(false); // turns off sync between C and C++ output streams(to increase output speed)
 
@@ -59,35 +50,11 @@ void widget::initializeGL() {
     // ------- load raw data -------
     layers.emplace_back();
     layers.back().opacity = 1.0f;
-    auto& textures = layers.back().textures;
-
-    lut.reset(new QOpenGLTexture(QOpenGLTexture::Target1D));
-    QOpenGLTexture & texture = *lut;
-    texture.setAutoMipMapGenerationEnabled(false);
-    texture.setSize(1024);
-    texture.setMipLevels(1);
-    texture.setMinificationFilter(QOpenGLTexture::Nearest);
-    texture.setMagnificationFilter(QOpenGLTexture::Nearest);
-    texture.setFormat(QOpenGLTexture::RGBA8_UNorm);
-    texture.allocateStorage();
-
-    QHash<quint64, quint32> gpuIds;
-    std::vector<std::array<uint8_t, 4>> colors;
-    decltype(gpuIds)::mapped_type highestId = 0;
-
-    for (auto & texture : boost::make_iterator_range(textures.data(), textures.data() + textures.num_elements())) {
-        delete texture;
-    }
-    textures.resize(boost::extents[supercubeedge][supercubeedge]);
-    for (auto & texture : boost::make_iterator_range(textures.data(), textures.data() + textures.num_elements())) {
-        texture = nullptr;
-    }
 
     std::vector<char> data;
+    int z = 0;
     for (int y = 0; y < supercubeedge; ++y)
     for (int x = 0; x < supercubeedge; ++x) {
-        QElapsedTimer time;
-        time.start();
         const auto factor = cpucubeedge / gpucubeedge;
         std::string path = "cubes/2012-03-07_AreaX14_mag1_x00" + std::to_string(29+x/factor) + "_y00" + std::to_string(52-y/factor) + "_z0023.raw";
 //        std::string path = "C:/New folder/cubes/2012-03-07_AreaX14_mag1_x00" + std::to_string(29+x) + "_y00" + std::to_string(52-y) + "_z0023.raw";
@@ -102,84 +69,53 @@ void widget::initializeGL() {
             std::cout << path << " failed" << std::endl;
         }
 
-        textures[y][x] = new QOpenGLTexture(QOpenGLTexture::Target3D);
-        QOpenGLTexture & texture = *textures[y][x];
-        texture.setAutoMipMapGenerationEnabled(false);
-        texture.setSize(gpucubeedge, gpucubeedge, gpucubeedge);
-        texture.setMipLevels(1);
-        texture.setMinificationFilter(QOpenGLTexture::Linear);
-        texture.setMagnificationFilter(QOpenGLTexture::Linear);
-        texture.setFormat(QOpenGLTexture::R8_UNorm);
-        texture.setWrapMode(QOpenGLTexture::ClampToEdge);
-        texture.allocateStorage();
-
-        boost::multi_array_ref<char, 3> cube(data.data(), boost::extents[cpucubeedge][cpucubeedge][cpucubeedge]);
+        layers.back().textures[QVector3D(x, y, z)].reset(new gpu_raw_cube(gpucubeedge));
+        //boost::multi_array_ref<std::uint64_t, 3> cube(reinterpret_cast<std::uint64_t*>(data.data()), boost::extents[cpucubeedge][cpucubeedge][cpucubeedge]);
+        boost::multi_array_ref<std::uint8_t, 3> cube(reinterpret_cast<std::uint8_t*>(data.data()), boost::extents[cpucubeedge][cpucubeedge][cpucubeedge]);
         const auto x_offset = gpucubeedge * (x % factor);
         const auto y_offset = gpucubeedge * (y % factor);
         const auto z_offset = 0;
         using range = boost::multi_array_types::index_range;
         const auto view = cube[boost::indices[range(0+z_offset,gpucubeedge+z_offset)][range(cpucubeedge-gpucubeedge-y_offset,cpucubeedge-0-y_offset)][range(0+x_offset,gpucubeedge+x_offset)]];
-        std::vector<char> data2;
-        for (const auto & d2 : view)
-        for (const auto & d1 : d2)
-        for (const auto & elem : d1) {
-            const auto it = gpuIds.find(elem);
-            const auto existing = it != std::end(gpuIds);
-            const auto id = existing ? *it : (gpuIds[elem] = highestId++);
-            data2.emplace_back(id);
-            if (!existing) {
-                std::uint8_t idc = id;
-                colors.push_back({{idc, 0, 0, 255}});
-            }
-        }
-        colors.resize(lut->width() * lut->height() * lut->depth());
-
-        texture.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, data2.data());
-        lut->setData(QOpenGLTexture::RGBA, QOpenGLTexture::UInt32_RGBA8_Rev, colors.data());
+        static_cast<gpu_raw_cube*>(layers.back().textures[QVector3D(x, y, z)].get())->generate(view);
     }
 
     // ------- load overlay data -------
     layers.emplace_back();
     layers.back().opacity = 0.5f;
     layers.back().isOverlayData = true;
-    auto& otextures = layers.back().textures;
 
-    for (auto & texture : boost::make_iterator_range(otextures.data(), otextures.data() + otextures.num_elements())) {
-        delete texture;
-    }
-    otextures.resize(boost::extents[supercubeedge][supercubeedge]);
-    for (auto & texture : boost::make_iterator_range(otextures.data(), otextures.data() + otextures.num_elements())) {
-        texture = nullptr;
-    }
+    for (int y = 0; y < supercubeedge; ++y)
+    for (int x = 0; x < supercubeedge; ++x) {
+        const auto factor = cpucubeedge / gpucubeedge;
+        std::string path = "/run/media/mobile/00AAEB91AAEB8210/New folder/cubes/2012-03-07_AreaX14_mag1_x00" + std::to_string(29+x/factor) + "_y00" + std::to_string(52-(y/factor)) + "_z0023.raw.segmentation.raw";
+        std::ifstream file(path, std::ios_base::binary);
+        data.resize(std::pow(cpucubeedge, 3)*8);
+        if (file) {
+            file.read(data.data(), data.size());
+        } else {
+            std::fill(std::begin(data), std::end(data), 127);
+            std::cout << path << " failed" << std::endl;
+        }
 
-    std::vector<std::uint16_t> overlay_data;
-    for (int sy = 0; sy < supercubeedge; ++sy)
-    for (int sx = 0; sx < supercubeedge; ++sx) {
-        overlay_data.resize(std::pow(cpucubeedge, 3));
-        std::fill(std::begin(overlay_data), std::end(overlay_data), 1338);
-
-        otextures[sy][sx] = new QOpenGLTexture(QOpenGLTexture::Target3D);
-        QOpenGLTexture & otexture = *otextures[sy][sx];
-        otexture.setAutoMipMapGenerationEnabled(false);
-        otexture.setSize(cpucubeedge, cpucubeedge, cpucubeedge);
-        otexture.setMipLevels(1);
-        otexture.setMinificationFilter(QOpenGLTexture::Linear);
-        otexture.setMagnificationFilter(QOpenGLTexture::Linear);
-        otexture.setFormat(QOpenGLTexture::R16_UNorm);
-        otexture.allocateStorage();
-        otexture.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt16, overlay_data.data());
+        layers.back().textures[QVector3D(x, y, z)].reset(new gpu_lut_cube(gpucubeedge));
+        boost::multi_array_ref<std::uint64_t, 3> cube(reinterpret_cast<std::uint64_t*>(data.data()), boost::extents[cpucubeedge][cpucubeedge][cpucubeedge]);
+        const auto x_offset = gpucubeedge * (x % factor);
+        const auto y_offset = gpucubeedge * (y % factor);
+        const auto z_offset = 0;
+        using range = boost::multi_array_types::index_range;
+        const auto view = cube[boost::indices[range(0+z_offset,gpucubeedge+z_offset)][range(cpucubeedge-gpucubeedge-y_offset,cpucubeedge-0-y_offset)][range(0+x_offset,gpucubeedge+x_offset)]];
+        static_cast<gpu_lut_cube*>(layers.back().textures[QVector3D(x, y, z)].get())->generate(view);
     }
 
     auto vertex_shader_code = R"shaderSource(
-    //#version 110
-
+    #version 110
     uniform mat4 model_matrix;
     uniform mat4 view_matrix;
     uniform mat4 projection_matrix;
     attribute vec3 vertex;
     attribute vec3 texCoordVertex;
     varying vec3 texCoordFrag;
-
     void main() {
         mat4 mvp_mat = projection_matrix * view_matrix * model_matrix;
         gl_Position = mvp_mat * vec4(vertex, 1.0f);
@@ -188,35 +124,13 @@ void widget::initializeGL() {
 
     raw_data_shader.addShaderFromSourceCode(QOpenGLShader::Vertex, vertex_shader_code);
     raw_data_shader.addShaderFromSourceCode(QOpenGLShader::Fragment, R"shaderSource(
-    //#version 110
-    uniform float texture_opacity;
-    uniform sampler3D textureCentral;
-    uniform sampler3D textureLeft;
-    uniform sampler3D textureRight;
-    uniform sampler3D textureTop;
-    uniform sampler3D textureBottom;
-    uniform sampler1D textureLUT;
-    uniform float cubeedgelength;
+    #version 110
+    uniform float textureOpacity;
+    uniform sampler3D texture;
     varying vec3 texCoordFrag;//in
     //varying vec4 gl_FragColor;//out
     void main() {
-//        gl_FragColor = vec4(texCoordFrag, texture_opacity);
-//        gl_FragColor = vec4(vec3(texture3D(textureCentral, texCoordFrag).r), texture_opacity);
-        float index = texture3D(textureCentral, texCoordFrag).r;
-        float size = 1024.0;//float(textureSize1D(textureLUT, 0));
-        index *= 256.0;//expand float to uint8 range
-        gl_FragColor = texture1D(textureLUT, (index + 0.5) / size);
-        gl_FragColor.a = texture_opacity;
-        //gl_FragColor = texelFetch(textureLUT, int(index), 0);//requires version 130
-//        vec4 left = texCoordFrag.x == 0.0f ? texture3D(textureLeft, vec3(cubeedgelength, texCoordFrag.yz)) : texture3D(textureCentral, texCoordFrag);
-//        vec4 right = texCoordFrag.x == cubeedgelength ? texture3D(textureRight, vec3(0, texCoordFrag.yz)) : texture3D(textureCentral, texCoordFrag);
-//        vec4 top = texCoordFrag.y == 0.0f ? texture3D(textureTop, vec3(texCoordFrag.x, cubeedgelength, texCoordFrag.z)) : texture3D(textureCentral, texCoordFrag);
-//        vec4 bottom = texCoordFrag.y == cubeedgelength ? texture3D(textureBottom, vec3(texCoordFrag.x, 0, texCoordFrag.z)) : texture3D(textureCentral, texCoordFrag);
-//        vec4 center = texture3D(textureCentral, texCoordFrag);
-//        center = mix(left, center, 0.5);
-//        center = mix(right, center, 0.5);
-//        center = mix(top, center, 0.5);
-//        gl_FragColor = mix(bottom, center, 0.5);
+        gl_FragColor = vec4(vec3(texture3D(texture, texCoordFrag).r), textureOpacity);
     })shaderSource");
 
     raw_data_shader.link();
@@ -224,17 +138,17 @@ void widget::initializeGL() {
 
     overlay_data_shader.addShaderFromSourceCode(QOpenGLShader::Vertex, vertex_shader_code);
     overlay_data_shader.addShaderFromSourceCode(QOpenGLShader::Fragment, R"shaderSource(
-    //#version 110
-
-    uniform float texture_opacity;
-    uniform sampler3D textureCentral;
+    #version 110
+    uniform float textureOpacity;
+    uniform sampler3D indexTexture;
+    uniform sampler1D textureLUT;
     varying vec3 texCoordFrag;//in
-
     void main() {
-        if(texture3D(textureCentral, texCoordFrag).r == 1337.0f / 65535.0f)
-            gl_FragColor = vec4(1.0f, 0.0f, 0.0, texture_opacity);
-        else
-            gl_FragColor = vec4(0.0f);
+        float index = texture3D(indexTexture, texCoordFrag).r;
+        float size = 1024.0;//float(textureSize1D(textureLUT, 0));
+        index *= 256.0;//expand float to uint8 range
+        gl_FragColor = texture1D(textureLUT, (index + 0.5) / size);
+        gl_FragColor.a = textureOpacity;
     })shaderSource");
 
     overlay_data_shader.link();
@@ -310,14 +224,7 @@ void widget::paintGL() {
     raw_data_shader.setUniformValue("model_matrix", modelMatrix);
     raw_data_shader.setUniformValue("view_matrix", viewMatrix);
     raw_data_shader.setUniformValue("projection_matrix", projectionMatrix);
-    raw_data_shader.setUniformValue("textureCentral", 0);
-    raw_data_shader.setUniformValue("textureLeft", 1);
-    raw_data_shader.setUniformValue("textureRight", 2);
-    raw_data_shader.setUniformValue("textureTop", 3);
-    raw_data_shader.setUniformValue("textureBottom", 4);
-    raw_data_shader.setUniformValue("textureLUT", 5);
-    const float cubeedgef = gpucubeedge;
-    raw_data_shader.setUniformValue("cubeedgelength", cubeedgef);
+    raw_data_shader.setUniformValue("texture", 0);
 
     // overlay data shader
     overlay_data_shader.bind();
@@ -330,22 +237,29 @@ void widget::paintGL() {
     overlay_data_shader.setUniformValue("model_matrix", modelMatrix);
     overlay_data_shader.setUniformValue("view_matrix", viewMatrix);
     overlay_data_shader.setUniformValue("projection_matrix", projectionMatrix);
-    overlay_data_shader.setUniformValue("textureCentral", 0);
+    overlay_data_shader.setUniformValue("indexTexture", 0);
+    overlay_data_shader.setUniformValue("textureLUT", 1);
 
-    for(const auto& layer : layers) {
-        if(layer.enabled && layer.opacity >= 0.0f) {
-            if(layer.isOverlayData) {
+    for (auto & layer : layers) {
+        if (layer.enabled && layer.opacity >= 0.0f) {
+            if (layer.isOverlayData) {
                 overlay_data_shader.bind();
-                overlay_data_shader.setUniformValue("texture_opacity", layer.opacity);
+                overlay_data_shader.setUniformValue("textureOpacity", layer.opacity);
             } else {
                 raw_data_shader.bind();
-                raw_data_shader.setUniformValue("texture_opacity", layer.opacity);
+                raw_data_shader.setUniformValue("textureOpacity", layer.opacity);
             }
 
+            float z = 0;
             for (float y = 0; y < supercubeedge; ++y)
             for (float x = 0; x < supercubeedge; ++x) {
-                layer.textures[y][x]->bind(0);
-                lut->bind(5);
+                auto ptr = layer.textures[QVector3D(x, y, z)].get();
+                if (layer.isOverlayData) {
+                    static_cast<gpu_lut_cube*>(ptr)->cube.bind(0);;
+                    static_cast<gpu_lut_cube*>(ptr)->lut.bind(1);
+                } else {
+                    ptr->cube.bind(0);;
+                }
                 glDrawArrays(GL_QUADS, 4 * (y * supercubeedge + x), 4);
             }
         }
--- widget.h
+++ widget.h
@@ -11,12 +11,98 @@
 #include <QTimer>
 #include <QVBoxLayout>
 
+#include <boost/functional/hash.hpp>
 #include <boost/multi_array.hpp>
 #include <cstdint>
 #include <list>
+#include <unordered_map>
+
+namespace std {
+template<>
+struct hash<QVector3D> {
+    std::size_t operator()(const QVector3D & cord) const {
+        return boost::hash_value(std::make_tuple(cord.x(), cord.y(), cord.z()));
+    }
+};
+}
+
+class gpu_raw_cube {
+public:
+    QOpenGLTexture cube{QOpenGLTexture::Target3D};
+
+    gpu_raw_cube(int gpucubeedge) {
+        cube.setAutoMipMapGenerationEnabled(false);
+        cube.setSize(gpucubeedge, gpucubeedge, gpucubeedge);
+        cube.setMipLevels(1);
+        cube.setMinificationFilter(QOpenGLTexture::Linear);
+        cube.setMagnificationFilter(QOpenGLTexture::Linear);
+        cube.setFormat(QOpenGLTexture::R8_UNorm);
+        cube.setWrapMode(QOpenGLTexture::ClampToEdge);
+        cube.allocateStorage();
+    }
+
+    void generate(boost::multi_array_ref<std::uint8_t, 3>::const_array_view<3>::type view) {
+        std::vector<char> data;
+        for (const auto & d2 : view)
+        for (const auto & d1 : d2)
+        for (const auto & elem : d1) {
+            data.emplace_back(elem);
+        }
+        cube.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, data.data());
+    }
+};
+
+class gpu_lut_cube : public gpu_raw_cube {
+    using gpu_index = quint32;
+    QHash<quint64, gpu_index> id_to_lut_index;
+    gpu_index highest_index = 0;
+    std::vector<std::array<uint8_t, 4>> colors;
+public:
+    QOpenGLTexture lut{QOpenGLTexture::Target1D};
+
+    gpu_lut_cube(int gpucubeedge) : gpu_raw_cube(gpucubeedge) {
+        lut.setAutoMipMapGenerationEnabled(false);
+        lut.setSize(1024);
+        lut.setMipLevels(1);
+        lut.setMinificationFilter(QOpenGLTexture::Nearest);
+        lut.setMagnificationFilter(QOpenGLTexture::Nearest);
+        lut.setFormat(QOpenGLTexture::RGBA8_UNorm);
+        lut.allocateStorage();
+    }
+
+    void generate(boost::multi_array_ref<std::uint64_t, 3>::const_array_view<3>::type view) {
+        std::vector<char> data;
+        for (const auto & d2 : view)
+        for (const auto & d1 : d2)
+        for (const auto & elem : d1) {
+            const auto it = id_to_lut_index.find(elem);
+            const auto existing = it != std::end(id_to_lut_index);
+            const auto index = existing ? *it : (id_to_lut_index[elem] = highest_index++);//increment after assignment
+            data.emplace_back(index);
+            if (!existing) {
+                if (elem == 0) {
+                    colors.push_back({{0, 0, 0, 0}});
+                } else if (elem == 1) {
+                    colors.push_back({{255, 0, 0, 255}});
+                } else if (elem == 2) {
+                    colors.push_back({{0, 255, 0, 255}});
+                } else if (elem == 3) {
+                    colors.push_back({{0, 0, 255, 255}});
+                } else {
+                    std::uint8_t idc = elem;
+                    colors.push_back({{idc, 0, 0, 255}});
+                }
+            }
+        }
+        colors.resize(lut.width() * lut.height() * lut.depth());
+
+        cube.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, data.data());
+        lut.setData(QOpenGLTexture::RGBA, QOpenGLTexture::UInt32_RGBA8_Rev, colors.data());
+    }
+};
 
 struct TextureLayer {
-    boost::multi_array<QOpenGLTexture*, 2> textures;
+    std::unordered_map<QVector3D, std::unique_ptr<gpu_raw_cube>> textures;
     float opacity = 1.0f;
     bool enabled = true;
     bool isOverlayData = false;
@@ -48,7 +134,6 @@ class widget : public QOpenGLWidget, protected QOpenGLFunctions_2_0 {
     QOpenGLShaderProgram overlay_data_shader;
 public:
     widget();
-    ~widget();
 };
 
 #endif//WIDGET_H
